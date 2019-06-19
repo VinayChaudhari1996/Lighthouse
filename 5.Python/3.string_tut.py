b616ffa3296e04003883a2e0a555ed9a9f527f48
amarsengar <44390207+amarsengar@users.noreply.github.com>
2019-06-19 12:35:14 +0530

    string string slicing and builtin function

---
+++ 5.Python/3.string_tut.py
@@ -0,0 +1,22 @@
+
+# string string slicing and builtin function
+
+mystr = "python is programming language"
+print(len(mystr))
+print(mystr[:-1])
+
+print(mystr.endswith("age"))
+print(mystr.count("o"))
+print(mystr.capitalize())
+print(mystr.replace("is", "are"))
+print(mystr.title())
+
+
+my =  "THIS IS PYTHON"
+
+print(my.lower())
+print(my.title())
+
+print(my.split())
+
+#  there is more builtin function You can practice
